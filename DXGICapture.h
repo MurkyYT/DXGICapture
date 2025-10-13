cfb375cc8755f82640c4ecf51ad58a8e73aff615
Murky <71258936+MurkyYT@users.noreply.github.com>
2025-10-13 16:21:07 +0300

    chore: update DXGICapture.h

--- DXGICapture.h
+++ DXGICapture.h
@@ -9,12 +9,18 @@
 
 #define DXGICAPTURE_ALL_SCREENS -1
 
+#ifdef __cplusplus
 extern "C" {
-	DXGICAPTURE_API BOOL DXGI_InitCapture(void);
-	DXGICAPTURE_API void DXGI_DeInitCapture(void);
-	DXGICAPTURE_API HBITMAP DXGI_CaptureScreen(int index);
-	DXGICAPTURE_API DXGI_OUTPUT_DESC DXGI_GetOutputDescription(int index);
-	DXGICAPTURE_API HBITMAP DXGI_UpdateFrame(void);
-	DXGICAPTURE_API int DXGI_OutputsCount(void);
-	DXGICAPTURE_API BOOL DXGI_IsEnabled(void);
+#endif
+
+DXGICAPTURE_API BOOL DXGI_InitCapture(void);
+DXGICAPTURE_API void DXGI_DeInitCapture(void);
+DXGICAPTURE_API HBITMAP DXGI_CaptureScreen(int index);
+DXGICAPTURE_API DXGI_OUTPUT_DESC DXGI_GetOutputDescription(int index);
+DXGICAPTURE_API HBITMAP DXGI_UpdateFrame(void);
+DXGICAPTURE_API int DXGI_OutputsCount(void);
+DXGICAPTURE_API BOOL DXGI_IsEnabled(void);
+
+#ifdef __cplusplus
 }
+#endif
